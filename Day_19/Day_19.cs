8860aba0a11f3dcbda7c22378286ad7980bf3539
Troy Zhang <troy.zhang@teradyne.com>
2022-12-21 11:17:03 +0800

    completed day 20 and day 19 part 1, need to take a detour for 19.2

--- Day_19/Day_19.cs
+++ Day_19/Day_19.cs
@@ -1,4 +1,5 @@
-﻿namespace AoC2022
+﻿using System.Text.RegularExpressions;
+namespace AoC2022
 {    
     public class cDay_19 : WeekN
     {
@@ -9,6 +10,7 @@
             Title = $"--- Day {x}: xxxxxxxx xxxxxxxx ---";
             inputLines = ReadInput($"input_Day{x}.txt");
             //inputLines = ReadInput("");
+            //solution = new Part1(inputLines);
         }
         public string[] ReadInput(string? fileName)
         {
@@ -20,11 +22,199 @@
         }
         public override string Part1()
         {
-            return $"{x}.1 - {1}";
+            int part1 = 0;
+            for (int i = 0; i < inputLines.Length; i++)
+            {
+                Blueprint B = new(inputLines[i], 24);
+                B.Play();
+                part1 += B.BestScore() * (i+1);
+                Console.WriteLine(part1);
+            }
+            return $"{x}.1 - {part1}";
         }
         public override string Part2()
         {
-            return $"{x}.2 - {2}";
+            int part2 = 1;
+            for (int i = 2; i >= 0 ; i--)
+            {
+                Blueprint B = new(inputLines[i], 32);
+                B.Play();
+                part2 *= B.BestScore();
+                Console.WriteLine(part2);
+            }
+            return $"{x}.2 - {part2}";
+        }
+        class resource
+        {
+            public int ore;
+            public int clay;
+            public int obsidian;
+            public int geode;
+            public resource(int ore, int clay, int obsidian, int geode)
+            {
+                this.ore = ore;
+                this.clay = clay;
+                this.obsidian = obsidian;
+                this.geode = geode;
+            }
+            public override string ToString()
+            {
+                return $"{ore},{clay},{obsidian},{geode}";
+            }
+            public bool Affordable(resource cost)
+            {
+                return !(ore < cost.ore || clay < cost.clay || obsidian < cost.obsidian);
+            }
+            public void Spend(resource cost)
+            {
+                ore -= cost.ore;
+                clay -= cost.clay;
+                obsidian -= cost.obsidian;
+                geode -= cost.geode;
+            }
+            public void Produce(resource production)
+            {
+                ore += production.ore;
+                clay += production.clay;
+                obsidian += production.obsidian;
+                geode += production.geode;
+            }
+            public static resource operator +(resource A, resource B)
+            {
+                resource result = new resource(A.ore+B.ore, A.clay+B.clay, A.obsidian+B.obsidian, A.geode+B.geode);
+                return result;
+            }
+            public static resource operator -(resource A, resource B)
+            {
+                resource result = new resource(A.ore - B.ore, A.clay - B.clay, A.obsidian - B.obsidian, A.geode - B.geode);
+                return result;
+            }
+        }
+        class Blueprint
+        {
+            string ID;
+            int time;
+            static Dictionary<string, Blueprint> History = new();
+            static Dictionary<string, resource> RobotPrice =new();
+            static int TimeLimit = 0;
+            public resource Balance;
+            public resource Robots;
+            Dictionary<string,Blueprint> MultiUniverse =new();
+            public Blueprint(string line, int PlayTime)
+            {
+                //Blueprint 1: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 16 clay. Each geode robot costs 3 ore and 9 obsidian.
+                Match m = Regex.Match(line, @"Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.");
+                //ID = int.Parse(m.Groups[1].Value);
+                int ore_ore = int.Parse(m.Groups[2].Value);
+                int clay_ore = int.Parse(m.Groups[3].Value);
+                int obsidian_ore = int.Parse(m.Groups[4].Value);
+                int obsidian_clay = int.Parse(m.Groups[5].Value);
+                int geode_ore = int.Parse(m.Groups[6].Value);
+                int geode_obsidian = int.Parse(m.Groups[7].Value);
+
+                time = 0;
+                Balance = new(0, 0, 0, 0);
+                Robots = new(1, 0, 0, 0);
+                ID = ToString();
+
+                History.Clear();
+                RobotPrice = new Dictionary<string, resource>();
+                RobotPrice.Add("ore", new resource(ore_ore, 0, 0, 0));
+                RobotPrice.Add("clay", new resource(clay_ore, 0, 0, 0));
+                RobotPrice.Add("obsidian", new resource(obsidian_ore, obsidian_clay, 0, 0));
+                RobotPrice.Add("geode", new resource(geode_ore, 0, geode_obsidian, 0));
+                TimeLimit = PlayTime;
+            }
+            public Blueprint(Blueprint lastMinute, resource robotBuilt, resource resourceChange, int time)
+            {
+                this.time = time;
+                Balance = lastMinute.Balance - resourceChange - robotBuilt;
+                Robots  = lastMinute.Robots + robotBuilt;
+                ID = ToString();
+            }
+            public override string ToString()
+            {
+                return $"{time};{Balance};{Robots}";
+            }
+            public string ToString(int time)
+            {
+                return $"{time};{Balance};{Robots}";
+            }
+            public void Play()
+            {
+                List<Blueprint> PossibleFutures = new List<Blueprint>();
+                for (int t = time; t < TimeLimit; t++)// && !MultiUniverse.Contains(ToString()))
+                {
+                    bool Affordable_geode = Balance.Affordable(RobotPrice["geode"]);
+                    bool Affordable_obsidian = Balance.Affordable(RobotPrice["obsidian"]);
+                    bool Affordable_clay = Balance.Affordable(RobotPrice["clay"]);
+                    bool Affordable_ore = Balance.Affordable(RobotPrice["ore"]);
+                    string NewID;
+
+                    if (Affordable_clay && Affordable_ore && Affordable_obsidian && Affordable_geode)
+                        break;
+
+                    Balance += Robots;
+                    NewID = ToString(t + 1);
+                    if (!History.ContainsKey(NewID))
+                        History.Add(NewID, this);
+                    else
+                        break;
+
+                    if (!Affordable_geode && Balance.Affordable(RobotPrice["geode"]))
+                    {
+                        Blueprint Universe = new Blueprint(this, new resource(0, 0, 0, 1), RobotPrice["geode"], t + 1);
+                        NewID = Universe.ToString();
+                        MultiUniverse.Add(NewID, Universe);
+                        //if (!History.ContainsKey(NewID))
+                        //    History.Add(NewID, this);
+                        Universe.Play();
+                        //break;
+                    }
+                    //
+                    if (!Affordable_obsidian && Balance.Affordable(RobotPrice["obsidian"]))
+                    {
+                        Blueprint Universe = new Blueprint(this, new resource(0, 0, 1, 0), RobotPrice["obsidian"], t + 1);
+                        NewID = Universe.ToString();
+                        MultiUniverse.Add(NewID, Universe);
+                        //if (!History.ContainsKey(NewID))
+                        //    History.Add(NewID, this);
+                        Universe.Play();
+                    }
+                    //
+                    if (!Affordable_clay && Balance.Affordable(RobotPrice["clay"]))
+                    {
+                        Blueprint Universe = new Blueprint(this, new resource(0, 1, 0, 0), RobotPrice["clay"], t + 1);
+                        NewID = Universe.ToString();
+                        MultiUniverse.Add(NewID, Universe);
+                        //if (!History.ContainsKey(NewID))
+                        //    History.Add(NewID, this);
+                        Universe.Play();
+                    }
+                    //
+                    if (!Affordable_ore && Balance.Affordable(RobotPrice["ore"]))
+                    {
+                        Blueprint Universe = new Blueprint(this, new resource(1, 0, 0, 0), RobotPrice["ore"], t + 1);
+                        NewID = Universe.ToString();
+                        MultiUniverse.Add(NewID, Universe);
+                        //if (!History.ContainsKey(NewID))
+                        //    History.Add(NewID, this);
+                        Universe.Play();
+                    }
+
+                }
+            }
+            public int BestScore()
+            {
+                int this_best = Balance.geode;
+                foreach (Blueprint Universe in MultiUniverse.Values)
+                {
+                    int another_best = Universe.BestScore();
+                    if (another_best > this_best)
+                        this_best = another_best;
+                }
+                return this_best;
+            }
         }
     }
 }
---
+++ Day_20/Day_20.cs
@@ -0,0 +1,148 @@
+﻿namespace AoC2022
+{    
+    public class cDay_20 : WeekN
+    {
+        readonly string[] inputLines;
+        readonly int x = 20; // x = [1..25]
+        Loop InfiniteLoop;
+
+        public cDay_20()
+        {
+            Title = $"--- Day {x}: xxxxxxxx xxxxxxxx ---";
+            inputLines = ReadInput($"input_Day{x}.txt");
+            //inputLines = ReadInput("");
+            
+        }
+        public string[] ReadInput(string? fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                fileName = "input_sample.txt";
+            }
+            return File.ReadAllLines(fileName);
+        }
+        public override string Part1()
+        {
+            InfiniteLoop = new Loop(inputLines, 1);
+            foreach (LoopItem item in InfiniteLoop)
+            {
+                item.Move(item.Value);
+            }
+            long x1000 = (InfiniteLoop.GetItemAt(1000).Value);
+            long x2000 = (InfiniteLoop.GetItemAt(2000).Value);
+            long x3000 = (InfiniteLoop.GetItemAt(3000).Value);
+
+            return $"{x}.1 - {x1000+x2000+x3000}";
+        }
+        public override string Part2()
+        {
+            InfiniteLoop = new(inputLines, 811589153);
+            for (int i = 0; i < 10; i++)
+            foreach (LoopItem item in InfiniteLoop)
+            {
+                item.Move(item.Value);
+            }
+            long x1000 = (InfiniteLoop.GetItemAt(1000).Value);
+            long x2000 = (InfiniteLoop.GetItemAt(2000).Value);
+            long x3000 = (InfiniteLoop.GetItemAt(3000).Value);
+
+            return $"{x}.2 - {x1000 + x2000 + x3000}";
+        }
+        class LoopItem
+        {
+            public long Value;
+            public LoopItem Previous;
+            public LoopItem Next;
+            public LoopItem(long value)
+            {
+                Value = value;
+                this.Previous = this;
+                this.Next = this;
+            }
+            public LoopItem(long value, LoopItem before, LoopItem after)
+            {
+                Value = value;
+                this.Previous = before;
+                this.Next = after;
+            }
+            void MoveBackward(long steps)
+            {
+                LoopItem Before = this;
+                for( int i = 0; i < steps; i++)
+                    Before= Before.Previous;
+                LoopItem After = Next;
+
+                this.Previous.Next = this.Next;
+                this.Next.Previous = this.Previous;
+                //
+                this.Previous = Before.Previous;
+                this.Next = Before;
+                //
+                Before.Previous.Next = this;
+                Before.Previous = this;
+
+
+            }
+            void MoveForeward(long steps)
+            {
+                LoopItem Before = Previous;
+                LoopItem After = this;
+                for (int i = 0; i < steps; i++)
+                    After = After.Next;
+                //
+                this.Next.Previous = this.Previous;
+                this.Previous.Next = this.Next;
+                //
+                this.Next = After.Next;
+                this.Previous = After;
+                //
+                After.Next.Previous = this;                
+                After.Next = this;
+            }
+            public void Move(long steps)
+            {
+                steps %= 4999;         
+                if (steps < 0)
+                    MoveBackward(-steps);                   
+                else if (steps > 0)
+                    MoveForeward(steps);
+            }
+        }
+        class Loop : List<LoopItem>
+        {
+            public LoopItem Head;
+            public Loop(string[] inputLines,long Ratio )
+            {
+                Head = new LoopItem(long.Parse(inputLines[0]) * Ratio);
+                Add(Head);
+                for (int i = 1; i < inputLines.Length; i++)
+                {
+                    InsertItem(long.Parse(inputLines[i]) * Ratio);
+                }
+            }
+            public void InsertItem(long n)
+            {
+                LoopItem N = new LoopItem(n,Head.Previous,Head);
+                Head.Previous.Next = N;
+                Head.Previous = N;
+                Add(N);
+            }
+            public LoopItem GetItemAt(int Index)
+            {
+                LoopItem I = Head;
+                while (I.Value != 0)
+                    I = I.Next;
+                for (int i = 0; i < Index % (this.Count); i++)
+                    I = I.Next;
+                return I;
+            }
+            public override string ToString()
+            {
+                string output = $"{Head.Value}";
+                for (LoopItem i = Head.Next; i != Head; i = i.Next)
+                    output += $", {i.Value}";
+                return output;
+            }
+        }
+    }
+}
