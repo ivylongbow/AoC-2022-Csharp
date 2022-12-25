812e9c6e96394cdd99a675e6bcd44f4d461df35d
Troy Zhang <troy.zhang@teradyne.com>
2022-12-25 13:45:44 +0800

    Day 25 finished, Day19.2 still working...

--- Day_25/Day_25.cs
+++ Day_25/Day_25.cs
@@ -6,7 +6,7 @@
         readonly int x = 25; // x = [1..25]
         public cDay_25()
         {
-            Title = $"--- Day {x}: xxxxxxxx xxxxxxxx ---";
+            Title = $"--- Day {x}: Full of Hot Air ---";
             inputLines = ReadInput($"input_Day{x}.txt");
             //inputLines = ReadInput("");
         }
@@ -20,11 +20,75 @@
         }
         public override string Part1()
         {
-            return $"{x}.1 - {1}";
+            long Sum = 0;
+            foreach(string line in inputLines)
+            {
+                Numbers number = new Numbers(line);
+                Sum += number.Decimal;
+                Console.WriteLine(number.Decimal);
+            }
+            Console.WriteLine(Sum);
+            return $"{x}.1 - {Numbers.ToSNAFU(Sum)}";
         }
         public override string Part2()
         {
-            return $"{x}.2 - {2}";
+            return $"{x}.2 - {"Merry Christmas! Ho~Ho~Ho~~!"}";
+        }
+        class Numbers
+        {
+            public long Decimal = 0;
+            string SNAFU = "";
+            public Numbers(string SNAFU)
+            {
+                this.SNAFU = SNAFU;
+                foreach(char c in SNAFU)
+                {
+                    Decimal *= 5;
+                    switch (c)
+                    {
+                        case '2':
+                            Decimal += 2;
+                            break;
+                        case '1':
+                            Decimal += 1;
+                            break;
+                        case '-':
+                            Decimal -= 1;
+                            break;
+                        case '=':
+                            Decimal -= 2;
+                            break;
+                    }
+                }
+            }
+            public static string ToSNAFU(long Decimal)
+            {
+                string SNAFU = "";
+                while (Decimal != 0)
+                {
+                    long r = (Decimal + 2) % 5 - 2;
+                    Decimal = (Decimal - r) / 5;
+                    switch (r)
+                    {
+                        case 0:
+                            SNAFU = "0" + SNAFU;
+                            break;
+                        case 1:
+                            SNAFU = "1" + SNAFU;
+                            break;
+                        case 2:
+                            SNAFU = "2" + SNAFU;
+                            break;
+                        case -1:
+                            SNAFU = "-" + SNAFU;
+                            break;
+                        case -2:
+                            SNAFU = "=" + SNAFU;
+                            break;
+                    }
+                }
+                return SNAFU;
+            }
         }
     }
 }
